d1c815e20c27421109465124bff48c26e5bc910e
MartinIthurbide <57947420+MartinIthurbide@users.noreply.github.com>
2023-04-05 18:09:35 +0200

    beau background de toolbar pour monsieur Alexandre

--- src/main/java/xshape/toolbar/ToolBar.java
+++ src/main/java/xshape/toolbar/ToolBar.java
@@ -17,15 +17,17 @@ public class ToolBar extends ShapeGroup {
     ShapeFactory factory = new ShapesFactory();
 
     public ToolBar(Renderer r) {
-        // draw line
-        Point2D startLine = new Point2D.Double(70, 0);
-        Point2D endLine = new Point2D.Double(70, 500);
-        r.drawLine(startLine, endLine, MyColor.BLACK);
 
-        // draw shapes
         ShapeFactory shapeFactory = new ShapesFactory();
+
+        // draw container
+        add(shapeFactory.createRectangle(0,0,500,70,MyColor.GRAY));
+        // draw shapes
         add(shapeFactory.createRectangle(10,10,20,40,MyColor.RED));
         add(shapeFactory.createDefaultPolygon(10,50,50,40,MyColor.BLACK));
+
+        // draw trash
+
 
     }
 
