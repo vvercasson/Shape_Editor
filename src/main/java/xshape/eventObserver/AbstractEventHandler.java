5b3bc7a8ae0db9bdd7dcc3029fa395e79b2bf5a2
aducos003 <alex.mannant@gmail.com>
2023-04-27 00:28:22 +0200

    fix : shape tool bar vers tool bar = rien + fix bug shape tool bar drag

--- src/main/java/xshape/eventObserver/AbstractEventHandler.java
+++ src/main/java/xshape/eventObserver/AbstractEventHandler.java
@@ -152,23 +152,28 @@ public abstract class AbstractEventHandler {
         }
         // RELEASE ON TOOLBAR
         else if (renderer.getShapeToolbar().getBackground().belongsTo(originClicked)) {
-            if (renderer.getSelectedShapes().size() > 1) {
-                ShapeGroup g = new ShapeGroup();
-                for (Shape s : renderer.getSelectedShapes()) {
-                    System.out.println("More then one shape dropped in toolbar");
-                    g.add(s);
-                    renderer.getShapeToolbar().addShapeToToolbar(g.resize(150));
+            if(renderer.getSelectedToolBarShape()==null){
+                if (renderer.getSelectedShapes().size() > 1) {
+                    ShapeGroup g = new ShapeGroup();
+                    for (Shape s : renderer.getSelectedShapes()) {
+                        System.out.println("More then one shape dropped in toolbar");
+                        g.add(s);
+                        renderer.getShapeToolbar().addShapeToToolbar(g.resize(150));
+                        refreshToolBarSave();
+                    }
+                } else {
+                    System.out.println("Only one shape selected");
+                    renderer.getShapeToolbar().addShapeToToolbar(renderer.getSelectedShapes().get(0).resize(150));
+                    renderer.getShapes().remove(renderer.getSelectedShapes().get(0));
                     refreshToolBarSave();
-                }
-            } else {
-                System.out.println("Only one shape selected");
-                renderer.getShapeToolbar().addShapeToToolbar(renderer.getSelectedShapes().get(0).resize(150));
+    
+                }  
+            }
+            else if (renderer.getSelectedToolBarShape()!=null){
                 renderer.getShapes().remove(renderer.getSelectedShapes().get(0));
-                refreshToolBarSave();
-
             }
         }
-
+        observer.updateUnselectedShape(renderer.getSelectedShapes().get(0));
         renderer.refreshCanva();
     }
 
