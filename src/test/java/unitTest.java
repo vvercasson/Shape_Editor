b8d05830880fe388a0ab0fd7688670aa9592b60d
Vercasson Victor <vercasson1605@gmail.com>
2023-04-26 17:28:33 +0200

    add de 2 test

--- src/test/java/unitTest.java
+++ src/test/java/unitTest.java
@@ -1,4 +1,5 @@
 import java.awt.geom.Point2D;
+import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -7,6 +8,7 @@ import xshape.shapes.*;
 import xshape.utils.MyColor;
 
 public class unitTest {
+
     @Test
     public void translateShapeTest() {
         ShapesFactory f = new ShapesFactory();
@@ -21,6 +23,39 @@ public class unitTest {
         Rectangle s = f.createDefaultRectangle(0, 0);
         s.setColor(MyColor.RED);
         assert (s.getColor() == MyColor.RED);
+    }
+
+    @Test
+    public void changeShapeOpacityTest() {
+        ShapesFactory f = new ShapesFactory();
+        Rectangle s = f.createDefaultRectangle(0, 0);
+        s.setOpacity(125);
+        assert (s.getColor().getOpacity() == 125);
+    }
+
+    @Test
+    public void createShapeGroup() {
+        // Init
+        int expectedNumberOfShape = 2;
+        ShapeGroup sg = new ShapeGroup();
+        ShapesFactory f = new ShapesFactory();
+
+        // Adding shapes to group
+        Shape r = f.createDefaultRectangle(0, 0);
+        Shape r2 = f.createDefaultRectangle(100, 100);
+        sg.add(r);
+        sg.add(r2);
+
+        // Counting
+        Iterator<Shape> it = sg.iterator();
+        int shapeCounter = 0;
+        while (it.hasNext()) {
+            it.next();
+            ++shapeCounter;
+        }
+
+        // Checking
+        assert (shapeCounter == expectedNumberOfShape);
     }
 
 }
